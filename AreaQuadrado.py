1c656f1dc568e28e95662871425dd8fdd30d8c07
MauricioApCastro <castromac6367@gmail.com>
2023-11-20 08:45:15 -0300

    inicioinicio

---
+++ AreaQuadrado.py
@@ -0,0 +1,4 @@
+aresta = float(input("Digite a largura ou comprimento: "))
+area = aresta * 2
+dobro = area * 2
+print(f"A área quadrado é: {area:.2f} e o dobro dessa area é: {dobro:.2f}")
---
+++ Salario.py
@@ -0,0 +1,4 @@
+salHora = float(input("Quanto você ganha por hora?: R$"))
+hora = int(input("Quantas horas você trabalhou?: "))
+salario = salHora * hora
+print(f"Seu salário será de R${salario:.2f} ")
